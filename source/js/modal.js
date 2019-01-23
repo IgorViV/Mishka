9893737f3383eea808bee791d8c37439f94d7bcb
IgorViV <iupgrader@mail.ru>
2019-01-23 13:43:24 +0300

    Добавлены функции открытия/закрытия модального окна

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,7 +1,7 @@
 var pageModal = document.querySelector('.page-modal');
-var buttonOrder = document.querySelector('.promo__button-order');
 var modalSubmit = pageModal.querySelector('.page-modal__submit');
-var videoButtonOrder = document.querySelector('.video-production__button-order');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 function onModalOpen () {
   pageModal.classList.remove('page-modal--hidden');
@@ -11,16 +11,51 @@ function onModalClose () {
   pageModal.classList.add('page-modal--hidden');
 };
 
-buttonOrder.addEventListener('click', function () {
-  pageModal.classList.remove('page-modal--hidden');
-});
+function isEscEvent (evt, action) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    action();
+  }
+};
 
-videoButtonOrder.addEventListener('click', function () {
-  pageModal.classList.remove('page-modal--hidden');
-});
+function isEnterEvent (evt, action) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    action();
+  }
+};
+
+if (document.querySelector('.promo__button-order')) {
+  var buttonOrder = document.querySelector('.promo__button-order');
+  buttonOrder.addEventListener('click', function () {
+    pageModal.classList.remove('page-modal--hidden');
+  });
+}
+
+if (document.querySelector('.catalog')) {
+  var catalog = document.querySelector('.catalog');
+  var catalogList = catalog.querySelector('.catalog__list');
+  var arrCatalogItems = catalogList.querySelectorAll('.catalog__list');
+
+  catalogList.addEventListener('click', function (evt) {
+    if (evt.target.tagName === 'BUTTON') {
+      if (evt.target.classList.contains('catalog__item-cart')) {
+        onModalOpen();
+      }
+    }
+  });
+}
 
 modalSubmit.addEventListener('click', function (evt) {
   evt.preventDefault();
 
-  pageModal.classList.add('page-modal--hidden');
+  onModalClose();
+});
+
+modalSubmit.addEventListener('keydown', function (evt) {
+
+  isEnterEvent (evt, onModalClose);
+});
+
+document.addEventListener('keydown', function (evt) {
+
+  isEscEvent (evt, onModalClose);
 });
